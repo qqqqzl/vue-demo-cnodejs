88713d21eb66426e944b955cf57d3275027e1d91
fordtin <qian000100@gmail.com>
2018-03-08 17:27:50 +0800

    在项目中使用jQuery

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Index from '@/page/index'
 import Content from '@/page/content'
 import UET from '@/page/ue_test'
-
+import JQ from '@/page/jq_test'
 Vue.use(Router)
 
 export default new Router({
@@ -17,6 +17,9 @@ export default new Router({
     }, {
       path: '/ue',
       component: UET
+    }, {
+      path: '/jq',
+      component: JQ
     }
   ]
 })
